185b214dd74bb390a34d8ea8b957eaf0d9ed6ef5
kelvan Sun <kelvan.sun@gmail.com>
2013-03-18 10:54:41 +0800

    Init home cfg

---
+++ .emacs.d/config/a.02.dev-global.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path "~/.emacs.d/site-lisp/git-emacs-master/")
+
+(require 'git-emacs)
---
+++ .emacs.d/config/a.05.python.el
@@ -0,0 +1,3 @@
+(setq py-install-directory "~/.emacs.d/site-listp/python-mode/")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/python-mode")
+(require 'python-mode)
